00ebf1fc47923be77b68c62c57fdb17034a71d39
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2021-02-12 15:03:02 -0300

    Obtendo post por id

---
+++ src/main/java/com/leticiabarbosa/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.leticiabarbosa.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leticiabarbosa.workshopmongo.domain.Post;
+import com.leticiabarbosa.workshopmongo.repository.PostRepository;
+import com.leticiabarbosa.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElse(null);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+}
